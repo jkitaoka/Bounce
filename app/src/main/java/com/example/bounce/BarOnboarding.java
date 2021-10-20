407bc94c375c1b375491c7b483778b6ec77eb641
alyssamussar <71838619+alyssamussar@users.noreply.github.com>
2021-10-19 21:04:57 -0500

    add bar onboarding

---
+++ app/src/main/java/com/example/bounce/BarOnboarding.java
@@ -0,0 +1,14 @@
+package com.example.bounce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarOnboarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bar_onboarding);
+    }
+}
