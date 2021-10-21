ad043cb65d730ca230a59ad5c7ba098e10ed0e05
jkitaoka <jkitaoka@wisc.edu>
2021-10-21 10:29:54 -0500

    john push, indiv status + post status

---
+++ app/src/main/java/com/example/bounce/StatusIndividual.java
@@ -0,0 +1,14 @@
+package com.example.bounce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatusIndividual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_individual);
+    }
+}
