da1c7f27a3c855700fec8c17ed8435c09a831bee
alyssamussar <71838619+alyssamussar@users.noreply.github.com>
2021-12-11 19:54:54 -0600

    bar name from DB

--- app/src/main/java/com/example/bounce/BarSideMain.java
+++ app/src/main/java/com/example/bounce/BarSideMain.java
@@ -1,12 +1,10 @@
 package com.example.bounce;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BarSideMain extends AppCompatActivity {
 
